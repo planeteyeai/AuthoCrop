2ed8d550f975ecd66963a016916f1fe5e48fd0c5
mayurshinde2002 <mp149716@gmail.com>
2025-09-30 16:18:54 +0530

    pest meter Changes

---
+++ src/components/pestt/meter/Weeds.ts
@@ -0,0 +1,43 @@
+export const weedsData: Weed[] = [
+    {
+      name: "Hariali",
+      season: "February to May",
+      chemical: "Fenoxaprop-p-ethyl 9.3% EC",
+      dosage: "400 ml/acre in 150-200 l water",
+      organic: "N/A",
+    //   image: "https://images.pexels.com/photos/6985009/pexels-photo-6985009.jpeg?auto=compress&cs=tinysrgb&am…
+    },
+    {
+      name: "Congress grass",
+      season: "February to May",
+      chemical: "2,4-D Sodium Salt 80% WP",
+      dosage: "500-750 gm/acre in 150–200 L water",
+      organic: "N/A",
+    //   image: "https://images.pexels.com/photos/6985010/pexels-photo-6985010.jpeg?auto=compress&cs=tinysrgb&am…
+    },
+    {
+      name: "Rajgira",
+      season: "June to September",
+      chemical: "Atrazine 50% WP",
+      dosage: "500 gm/acre 150-200 water",
+      organic: "N/A",
+    //   image: "https://images.pexels.com/photos/6985011/pexels-photo-6985011.jpeg?auto=compress&cs=tinysrgb&am…
+    },
+    {
+      name: "Samkhalu",
+      season: "June to September",
+      chemical: "Pendimethalin 30% EC",
+      dosage: "750 to 1000 ml/acre in 150-200 water",
+      organic: "N/A",
+    //   image: "https://images.pexels.com/photos/6985012/pexels-photo-6985012.jpeg?auto=compress&cs=tinysrgb&am…
+    },
+    {
+      name: "Bathua",
+      season: "October to January",
+      chemical: "2,4-D Sodium Salt 80% WP",
+      dosage: "600-700 gm/acre 150-200 water",
+      organic: "N/A",
+    //   image: "https://images.pexels.com/photos/6985013/pexels-photo-6985013.jpeg?auto=compress&cs=tinysrgb&am…
+    }
+  ];
+   
